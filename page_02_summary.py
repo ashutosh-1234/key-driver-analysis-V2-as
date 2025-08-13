cff2a900bf1caee5373b7f94ce094f19419f3958
ashutosh-1234 <ashutosh.sinha.1996@gmail.com>
2025-08-13 12:47:01 +0530

    Update page_02_summary.py

--- page_02_summary.py
+++ page_02_summary.py
@@ -101,11 +101,7 @@ def generate_visualizations(filtered_df, outcomes, rep_attributes, perceptions,
                             x=clean_data,
                             nbins=7,
                             title=f"Distribution: {outcome}",
-                            color_discrete_sequence=['skyblue'],
-                            labels={
-                            'x': 'Distribution',  # x-axis label
-                            'y': 'Count'    # y-axis label
-                            }
+                            color_discrete_sequence=['skyblue']
                         )
                         fig.add_vline(x=clean_data.mean(), line_dash="dash", line_color="red",
                                     annotation_text=f"Mean: {clean_data.mean():.2f}")
